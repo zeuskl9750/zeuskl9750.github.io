4d399ccdb75e8c928307f69f0543a4bfae4eb2e6
zeuskl9750 <135724133lin@zohomail.cn>
2025-06-30 21:17:48 +0800

    Site updated: 2025-06-30 21:17:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/22/hello-world/","2021/08/20/123/"];function toRandomPost(){
+var posts=["2021/08/20/123/","2025/06/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
